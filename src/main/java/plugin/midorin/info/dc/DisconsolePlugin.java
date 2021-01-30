7450b05aa0634cc8077d0b22f631731b8ec950f8
Minidon0413 <48941476+Minidon0413@users.noreply.github.com>
2021-01-30 18:47:15 +0900

    Add files via upload

---
+++ src/main/java/plugin/midorin/info/dc/DisconsolePlugin.java
@@ -0,0 +1,17 @@
+package plugin.midorin.info.dc;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class DisconsolePlugin extends JavaPlugin
+{
+
+    @Override
+    public void onEnable()
+    {
+    }
+
+    @Override
+    public void onDisable()
+    {
+    }
+}
